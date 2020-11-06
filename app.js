2f28ff86037e449d156c3ab053fde68407a6fdb8
ky-lore <kclore3131@gmail.com>
2020-11-06 00:19:41 -0800

    First stable version release, local storage functions added and all events interactive and functioning as intended.

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ function applyTimeStyling() {
   for (i = 9; i <= 17; i++) {
     var setTime = parseInt(document.getElementById(`${i}`).textContent);
     var timeSlot = document.getElementById(`textarea-${i}`);
-    
+
     if (setTime < getCurrentHour()) {
       timeSlot.style.backgroundColor = "dimgray";
     } else if (setTime === getCurrentHour()) {
@@ -23,19 +23,34 @@ function applyTimeStyling() {
 
 applyTimeStyling();
 
-document.getElementById("saveBtn").addEventListener('click', (event) => {
-  console.log('aha')
-  //let btn = event.target.parentNode
-  let userInput = event.target.parentNode.getElementsByClassName("tbox")[0].value;
-  console.log(userInput)
-  let time = event.target.parentNode.getElementsByClassName("hour")[0].textContent.charAt(0);
-  console.log(time)
-  // console.log(btn.getElementsByClassName("tbox")[0].value)
-  //localStorage.setItem(time, userInput);
-})
+var hours = document.querySelectorAll(".saveBtn").length;
+
+for (i = 0; i < hours; i++) {
+  document.querySelectorAll(".saveBtn")[i].addEventListener('click', (event) => {
+    //console.log('aha')
+    //let btn = event.target.parentNode
+    let userInput = event.target.parentNode.getElementsByClassName("tbox")[0].value;
+    //console.log(userInput)
+    let time = event.target.parentNode.getElementsByClassName("hour")[0].textContent.slice(0, -3);
+    //console.log(time)
+    // console.log(btn.getElementsByClassName("tbox")[0].value)
+    localStorage.setItem(time, userInput);
+    console.log(localStorage)
+  })
+}
 
 
+function initializeLocalStorage() {
+  for (i = 9; i < 18; i++) {
+    let storedItem = window.localStorage.getItem(i)
+    let userInput = document.getElementsByClassName("tbox")[i - 9];
+    userInput.innerText = storedItem;
+    console.log(userInput)
+    console.log(storedItem)
+  }
+}
 
+initializeLocalStorage();
 
 // --GIVEN I am using a daily planner to create a schedule
 // --WHEN I open the planner
@@ -46,7 +61,7 @@ document.getElementById("saveBtn").addEventListener('click', (event) => {
 // --THEN each timeblock is color coded to indicate whether it is in the past, present, or future
 // --WHEN I click into a timeblock
 // --THEN I can enter an event
-// WHEN I click the save button for that timeblock
-// THEN the text for that event is saved in local storage
+// --WHEN I click the save button for that timeblock
+// --THEN the text for that event is saved in local storage
 // WHEN I refresh the page
 // THEN the saved events persist
